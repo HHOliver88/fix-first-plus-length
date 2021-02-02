de9538ce66a35192a9ad065a570de40107421f9f
Oliver Rosales <oliverjohnpr2013@gmail.com>
2021-02-02 23:26:10 +0800

    Fixes

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var App                 = Express();
 App.get('/', function(request, response) {
     function firstPlusLength(arr) {
         let first = arr[1];
-        let length = 5;
+        let length = this.length;
         let result = first + length;
 
         return result;
     }
     
+    let a = firstPlusLength([0,1,2,3,4]);
+    let b = firstPlusLength([3,0,2,5]);
+    let c = firstPlusLength([-5,0,2,5]);
+    let d = firstPlusLength([1, 2]);
+
     // should print out 5, 7, -1, 3
-    response.send("<h1>" + firstPlusLength([0,1,2,3,4]) + ", " + firstPlusLength([3,0,2,5]) + ", " + firstPlusLength([-5,0,2,5]) + ", " + firstPlusLength([1, 2]) + "</h1>");
+    response.send("<h1>" + a + ", " + b + ", " + c + ", " + d + "</h1>");
 })
 
 App.listen(3000, function() {});
